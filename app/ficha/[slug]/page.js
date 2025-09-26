2fece3b69f59ff2971af891bac040cdf307c4969
Mateus de Jesus <mateus191019@gmail.com>
2025-09-25 20:49:26 -0400

    refactor: remover código não utilizado e simplificar a função Ficha

--- app/ficha/[slug]/page.js
+++ app/ficha/[slug]/page.js
@@ -1,15 +1,7 @@
-"use client";
-
 import { use } from "react";
-import { useRouter } from "next/navigation";
 import FichaRPG from "../../components/FichaRPG";
-import { useAuth } from "../../contexts/AuthContext";
-import LoadingPage from "@/app/components/Loading";
 
-export default function Ficha(props) {
-  const { user, loadingPage } = useAuth();
-
-  const { slug } = use(props.params);
-
+export default function Ficha({ params }) {
+  const { slug } = use(params); // ✅ resolve a Promise
   return <FichaRPG idFicha={slug} />;
 }
